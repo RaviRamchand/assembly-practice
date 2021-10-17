4630bcae03b482fa11e2120970a75616bac67437
Ravi Ramchand <45878442+RaviRamchand@users.noreply.github.com>
2021-10-17 17:31:34 -0400

    Addition, Subtraction and Multiplication exercise

---
+++ woksheet2.asm
@@ -0,0 +1,70 @@
+;5+4+120-20 -> values given in decimal 
+
+MOV AL, 5d    
+MOV BL, 4d
+
+ADD AL,BL
+
+MOV CL, 120d
+
+ADD AL,CL              
+
+SUB AL, 20d  
+
+;(5 + 4) - (120 - 20) -> answer in al  
+
+MOV AL, 5d
+
+ADD AL, 4d
+
+MOV BL, 120d
+
+SUB BL, 20d
+
+SUB AL, BL 
+
+;(5 +4) * (100 - 20) -> answer ax
+
+MOV AL, 5d
+ADD AL, 4d
+
+MOV BL, 100d
+SUB BL,20d
+
+MUL BL
+
+;3 + ( 4 + 100) * ( 4 -5) -> answer in ax
+
+MOV AL, 4d
+ADD AL, 100d
+
+MOV BL, 4d
+SUB BL, 5d
+
+IMUL BL
+
+ADD AX,3  
+
+; (3 * 3) + 5 * ( 2 +5)
+
+MOV AL, 3
+MOV BL, 3
+
+; 3*3=9
+MUL BL
+
+; Move AL to BL
+MOV BL, AL  
+
+; Make AL 2 and add 5
+MOV AL, 2
+ADD AL, 5
+
+; Make CL 5
+MOV CL, 5
+
+; 5*7
+MUL CL 
+
+; 35 + 9
+ADD AL, BL
